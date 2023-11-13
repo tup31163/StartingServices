0047a1cbb25f3a3be6fb7dd9dd8e034c790379b5
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-11-13 15:26:48 -0500

    create service, add to manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.startingservices"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.startingservices"
---
+++ app/src/main/java/com/example/startingservices/MainService.kt
@@ -0,0 +1,11 @@
+package com.example.startingservices
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
