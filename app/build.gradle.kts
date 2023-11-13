223324a530ab5274b10fbc7a3e3f14d31dfb036c
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-11-13 15:47:03 -0500

    write service with coroutine, results displaying in logcat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/startingservices/MainService.kt
+++ app/src/main/java/com/example/startingservices/MainService.kt
@@ -3,10 +3,50 @@ package com.example.startingservices
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainService : Service() {
+    var startId = 0
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
 
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        val startValue = intent?.getStringExtra("KEY")?.toInt()
+        runTimer(startId, startValue)
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun runTimer(startId: Int, startValue: Int?) {
+        TimerThread(startId, startValue).start()
+    }
+
+    override fun onDestroy() {
+        Log.d("Service State", "STOPPED")
+        super.onDestroy()
+    }
+
+    inner class TimerThread(private val startId: Int, private val startValue: Int?) : Thread() {
+        override fun run() {
+
+            CoroutineScope(Dispatchers.Default).launch {
+                countdown(startValue)
+            }
+        }
+
+        private suspend fun countdown(startValue: Int?) {
+            if (startValue != null) {
+                for(i in startValue downTo 0) {
+                    Log.d("Countdown", i.toString())
+                    delay(1000)
+                }
+            }
+        }
+    }
 }
